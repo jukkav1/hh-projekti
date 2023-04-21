db885009965a74cb43b761dc98b97837bf564c29
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-04-21 19:23:02 +0300

    layout almost there

--- main.py
+++ main.py
@@ -20,28 +20,32 @@ class MainWindow(GridLayout):
     def __init__(self, **kwargs):
         """superia kutsutaan, jotta perusluokan (LoginScreen vai GridLayout?) toiminnot eivät ylikirjoitu kokonaan, me vaan tehdään oma init -konstruktori [lähde: kivy.org/doc/stable/guide/basic.html]"""
         super(MainWindow, self).__init__(**kwargs)
-        self.rows = 2
-        self.cols = 3
-
         self.label1 = Label(text="Velkam", color="#0000BB")
         self.add_widget(self.label1)
+        self.cols = 1
+
+        self.bottomgrid = GridLayout()
+        self.bottomgrid.cols = 3
 
         self.nappula1 = Button(text="Merkinnät", background_color="#AAAA00")
         self.nappula1.bind(on_release=self.callback)
-        self.add_widget(self.nappula1)
+        self.bottomgrid.add_widget(self.nappula1)
 
         self.nappula2 = Button(text="Alkuun")
         self.nappula2.bind(on_release=self.callback)
-        self.add_widget(self.nappula2)
+        self.bottomgrid.add_widget(self.nappula2)
 
         self.nappula3 = Button(text="Harjoitteet", background_color="#00AA00")
         self.nappula3.bind(on_release=self.callback)
-        self.add_widget(self.nappula3)
+        self.bottomgrid.add_widget(self.nappula3)
 
         # Quit -nappula toimii jo. Quit on punainen
         self.nappula4 = Button(text="Quit", background_color="#FF2020")
         self.nappula4.bind(on_release=exit)
         self.add_widget(self.nappula4)
+
+        self.add_widget(self.bottomgrid)
+
 
 class MainBackground(Widget):
     def __init__(self, **kwargs):
@@ -53,7 +57,7 @@ class MainBackground(Widget):
 class HH(App):
     # alustetaan (konstruktoidaan) juuriolio, pääkikkare, root widget, what ever
     def build(self):
-        # palautetaan ainoastaan Label -nimike, jonka tekstinä .. "hello world".
+        # palautetaan MainWindow
         return MainWindow()
 
 
