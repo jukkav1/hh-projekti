f9b79f7b8206d20b89c046eebf61b972a04d2250
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-04-21 13:26:45 +0300

    -env crap to be ignored by git

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.behaviors import ButtonBehavior
 from kivy.uix.textinput import TextInput
 
 
-class LoginScreen(GridLayout):
+class MainWindow(GridLayout):
     def callback(self, nappi):
         print("%s sanoo ouch!" % nappi.text)
 
     def __init__(self, **kwargs):
         """superia kutsutaan, jotta perusluokan (LoginScreen vai GridLayout?) toiminnot eivät ylikirjoitu kokonaan, me vaan tehdään oma init -konstruktori [lähde: kivy.org/doc/stable/guide/basic.html]"""
-        super(LoginScreen, self).__init__(**kwargs)
-        self.cols = 2
+        super(MainWindow, self).__init__(**kwargs)
+        self.cols = 3
         self.add_widget(Label(text="Nimi"))
         self.username = TextInput(multiline=False)
         self.add_widget(self.username)
@@ -40,14 +39,19 @@ class LoginScreen(GridLayout):
         self.nappula3.bind(on_release=self.callback)
         self.add_widget(self.nappula3)
 
+        # Quit -nappula toimii jo
+        self.nappula4 = Button(text="Quit", background_color="#FF2020")
+        self.nappula4.bind(on_release=exit)
+        self.add_widget(self.nappula4)
+
 
 # noudatetaan siis sääntöä, että "perusluokka" periytyy kivyn omasta App -luokasta.
-class EkaOhjelma(App):
+class HH(App):
     # alustetaan (konstruktoidaan) juuriolio, pääkikkare, root widget, what ever
     def build(self):
         # palautetaan ainoastaan Label -nimike, jonka tekstinä .. "hello world".
-        return LoginScreen()
+        return MainWindow()
 
 
 if __name__ == "__main__":
-    EkaOhjelma().run()
+    HH().run()
