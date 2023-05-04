300bfd34c5ebcd5c4e2ba9289ca08515cc6c88a2
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-05-04 09:09:50 +0300

    diary as its own file

--- diary.py
+++ diary.py
@@ -1,5 +1,9 @@
 from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
 
+kv = Builder.load_file("diary.kv")
 
 class Diary(Screen):
     """päivyri"""
+    def build(self):
+        return kv
