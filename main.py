f9745a811a1d858d838cd6d800bcaad282b9dbbe
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-05-15 17:28:37 +0300

    burgerivalikko testi

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ kivy.require("2.1.0")  # vaaditaan tietty kivy -versio
 """ kivy vaatii, että ohjelman "perusluokka" periytyy App -luokasta, joka luokka löytyy: kivy_asennushakemisto/kivy/app.py """
 
 # otetaan hyödyllisiä kirjastoja käyttöön
-from kivy.app import App
+from kivymd.app import MDApp
 from kivy.core.window import Window
 
 # Välilehtiä varten
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.screenmanager import Screen
 
 # jotta kv tiedosto saadaan luettua
 from kivy.lang import Builder
@@ -20,6 +20,20 @@ from kivy.core.audio import SoundLoader
 
 # Boxlayout "split" ruutua varten
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+
+# sivun scrollaus
+from kivymd.uix.scrollview import MDScrollView
+
+# pois pääikkunasta
+Window.size = (280, 650)
+
+
+# tehdään luokka navigoinnille
+class ContentNavigationDrawer(MDScrollView):
+    # jotta tunnistetaan elementit ja voidaan viitata niihin vapaammin https://kivy.org/doc/stable/api-kivy.properties.html
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
 
 
 class MainWindow(BoxLayout):
@@ -49,7 +63,7 @@ class Home(Screen):
 
 
 # noudatetaan siis sääntöä, että "perusluokka" periytyy kivyn omasta App -luokasta.
-class HH(App):
+class HH(MDApp):
     """PääAppi"""
 
     # alustetaan (konstruktoidaan) juuriolio, pääkikkare, root widget, what ever
