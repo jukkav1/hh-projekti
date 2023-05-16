7f907a3e731e0d5eb970fa88e56d779ab57f57e7
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-05-16 07:45:21 +0300

    add exercise.py

---
+++ exercise.py
@@ -0,0 +1,19 @@
+from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+
+# äänien toistoa varten
+from kivy.core.audio import SoundLoader
+
+class Exercise(Screen):
+    """harjoitukset"""
+    Builder.load_file("exercise/exercise.kv")
+
+    def play_sound(self):
+        """Äänen toistofunktio"""
+
+        sound = SoundLoader.load("sounds/test.ogg")
+        if sound:
+            sound.play()
+        else:
+            print("äänen toisto ei onnistu T_T")
+        pass
