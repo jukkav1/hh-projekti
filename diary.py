5209fd682342582742bde5a545949a462b51da09
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-05-17 21:14:19 +0300

    write avaa nyt päivyrinäytön

--- diary.py
+++ diary.py
@@ -1,6 +1,9 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
+# päivyrimoduuli
+from kivymd.uix.pickers import MDDatePicker
+
 
 class Diary(Screen):
     """päivyri"""
@@ -8,7 +11,9 @@ class Diary(Screen):
     Builder.load_file("diary/diary.kv")
 
     def write_diary(self):
-        self.ids.leibel.text = "kirjoittele"
+        # Päivyrinäyttö
+        date_dialog = MDDatePicker()
+        date_dialog.open()
 
     def read_diary(self):
         self.ids.leibel.text = "lueskele"
