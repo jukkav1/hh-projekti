d6a789db3fc1e85395223845197fcb5c5a2c0e8b
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-05-23 13:44:53 +0300

    house cleaning

--- main.py
+++ main.py
@@ -2,14 +2,10 @@ import kivy
 from diary import *
 from exercise import *
 
-kivy.require("2.1.0")  # vaaditaan tietty kivy -versio
+kivy.require("2.1.0")
 
-
-# otetaan hyödyllisiä kirjastoja käyttöön
 from kivymd.app import MDApp
 from kivy.core.window import Window
-
-# Välilehtiä varten
 from kivy.uix.screenmanager import Screen
 
 # jotta kv tiedosto saadaan luettua
@@ -17,21 +13,10 @@ from kivy.lang import Builder
 
 # Boxlayout "split" ruutua varten
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import ObjectProperty
 
-# sivun scrollaus
-from kivymd.uix.scrollview import MDScrollView
-
-# pois pääikkunasta
+# Kokoruutu vai ikkuna
 # Window.fullscreen = True
 Window.size = (280, 650)
-
-
-# tehdään luokka navigoinnille
-class ContentNavigationDrawer(MDScrollView):
-    # jotta tunnistetaan elementit ja voidaan viitata niihin vapaammin https://kivy.org/doc/stable/api-kivy.properties.html
-    screen_manager = ObjectProperty()
-    nav_drawer = ObjectProperty()
 
 
 class MainWindow(BoxLayout):
@@ -45,9 +30,6 @@ class Home(Screen):
 ######
 # Diary ja exercise importataan ulkopuolelta
 ######
-
-
-# kivy vaatii, että ohjelman "perusluokka" periytyy App -luokasta, joka luokka löytyy: kivy_asennushakemisto/kivy/app.py
 
 
 # noudatetaan siis sääntöä, että "perusluokka" periytyy kivyn omasta App -luokasta.
