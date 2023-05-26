a06290548114f9abe032b6c3c1aa4be526f7e041
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-05-26 08:45:34 +0300

    päivyrin popup

--- diary.py
+++ diary.py
@@ -1,7 +1,9 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.uix.popup import Popup
-from kivy.uix.button import Button
+
+# from kivy.uix.button import Button
+# from kivy.uix.textinput import TextInput
 
 
 class Diary(Screen):
@@ -11,13 +13,15 @@ class Diary(Screen):
 
     def write_diary(self):
         # Päivyrinäyttö
-        date_dialog = Popup()
-        date_dialog.add_widget(Button(text="tallenna", on_release=date_dialog.dismiss))
+        date_dialog = Poppi()
+        # date_dialog.add_widget(Button(text="tallenna", on_release=date_dialog.dismiss))
         date_dialog.open()
 
     def read_diary(self):
         self.ids.leibel.text = "lueskele"
 
+
+class Poppi(Popup):
     def tallenna(self):
         print("koitetaan tallentaa")
         self.dismiss()
