146ffe832ecd9285dfd82969b9c2707bfdea1213
Tharyuu <125351885+Tharyuu@users.noreply.github.com>
2023-06-14 12:44:50 +0300

    lisätty progress bar raakile

--- exercise.py
+++ exercise.py
@@ -3,19 +3,31 @@ from kivy.lang import Builder
 
 # äänien toistoa varten
 from kivy.core.audio import SoundLoader
+from kivy.properties import NumericProperty
+from kivy.clock import Clock
 
 
 class Exercise(Screen):
     """harjoitukset"""
 
     Builder.load_file("kv/exercise.kv")
+    length = NumericProperty(0.0)
+    progress = NumericProperty(0.0)
 
     def play_sound(self):
         """Äänen toistofunktio"""
-
         sound = SoundLoader.load("sounds/test.ogg")
         if sound:
+            self.length = sound.length
             sound.play()
+            # päivitetään sekunnin välein
+            self.prog_ev = Clock.schedule_interval(self.update_progress, 1.0)
         else:
             print("äänen toisto ei onnistu T_T")
-        pass
+
+    def update_progress(self, dt):  # ei toimi ilman kahta
+        if self.progress < self.length:
+            self.progress += 1
+        else:
+            self.progress = 0  # nollataan palkki
+            self.prog_ev.cancel()  # lopetataan päivittäminen
