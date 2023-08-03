4005e64e19eb23ae50509203bec6603934cb26f3
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-08-03 09:05:27 +0300

    kalenteritausta. Taru saa jatkaa.

--- datepicker.py
+++ datepicker.py
@@ -1,6 +1,12 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import Screen
+
+
+class DatePickerContainer(Screen):
+    pass
 
 
 class DatePicker(BoxLayout):
