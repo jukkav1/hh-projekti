12d69a251bb7e7106abd06885b9a0be74829bcfe
Tharyuu <taru.pyykko@outlook.com>
2023-08-16 15:41:10 +0300

    siistitty dbfunc.py

--- db/dbfunc.py
+++ db/dbfunc.py
@@ -1,79 +1,96 @@
-# tilaa varattu tietokantafunktioille
-import sqlite3
-from sqlite3 import Error
+# Tietokantamoduuli
+
+# Otetaan tärkeät kirjastot mukaan
+from sqlite3 import connect, Error
 from os.path import isfile
 
+# Määritellään kanta ja käytettävä taulu
 kanta = "db/kanta.db"
 taulu = "merkinnat2"
 
 
-def hae_lista(kk, yy) -> list:
-    print("kk", kk, "yy:", yy)
-
+def hae_lista(kk: int, yy: int) -> list:
+    """Hakee listan kuukauden merkinnöistä ja palauttaa sen listana"""
     merkinnat = []
-    conn = sqlite3.connect(kanta)
+
+    # tietokantayhteys
+    conn = connect(kanta)
     cursor = conn.cursor()
+
+    # jos yhteys onnistuu, kysytään koko kuukauden merkinnät kannasta ja laitetaan päivämäärät listaan.
     if isfile(kanta):
         try:
-            x = cursor.execute(f"SELECT * from {taulu} WHERE month IS {kk}")
-            for y in x.fetchall():
-                merkinnat.append(str(y[0]))
-            print("merkinnät kannassa:", merkinnat)
+            x = cursor.execute(f"SELECT * FROM {taulu} WHERE month IS {kk}")
+            for _ in x.fetchall():
+                merkinnat.append(str(_[0]))
             return merkinnat
 
-        except Error as e:
-            print(e)
+        # joku meni vikaan
+        except Error as err:
+            print(err)
             return -1
+
+        # suljetaan kanta
+        finally:
+            conn.close()
+
     else:
-        print("kakkaa täh?")
-
-    conn.close()
+        print("Tiedostoa ei löydy.")
 
 
-def tee_merkinta(paiva, kuukausi, vuosi, teksti="Jotain outoa tapahtui!") -> bool:
-    print(f"Koitetaan tehdä merkintä '{teksti}' päivälle {paiva}.{kuukausi}.")
-    conn = sqlite3.connect(kanta)
+def tee_merkinta(paiva: int, kuukausi: int, vuosi: int, teksti: str) -> bool:
+    """Funktio yrittää tehdä merkinnän päivämäärälle tietokantaan"""
+    conn = connect(kanta)
     cursor = conn.cursor()
+
+    # Koitetaan lisätä tauluun päivälle ja kuukaudelle tietty teksti
     try:
         cursor.execute(
             f"INSERT INTO {taulu}(date,month,text) VALUES ('{paiva}', '{kuukausi}', '{teksti}');"
         )
         conn.commit()
+        r = True
 
+    # Jos ei onnistu ..
     except Error as e:
         print("Joku meni pieleen: ", e)
+        r = False
 
+    # Lopuksi suljetaan kanta ja annetaan paluuarvo
     finally:
         conn.close()
+        return r
 
 
 def hae_merkinta(paiva: int, kuukausi: int, vuosi: int) -> list:
+    """Hakee yksittäisen merkinnän pop-upia varten"""
     merkinnat = []
-    conn = sqlite3.connect(kanta)
+
+    # yhteys kantaan
+    conn = connect(kanta)
     cursor = conn.cursor()
+
+    # jos kanta on, katso mitä sisältää
     if isfile(kanta):
         try:
             x = cursor.execute(
-                f"SELECT * from {taulu} WHERE month IS {kuukausi} and date IS {paiva}"
+                f"SELECT * FROM {taulu} WHERE month IS {kuukausi} AND date IS {paiva}"
             )
+
+            # laitetaan listaan ja palautetaan
             for y in x.fetchall():
                 merkinnat.append(y)
-            return merkinnat
+
+        # Joku meni pieleen
         except Error as e:
             print(e)
-            return -1
+            merkinnat = -1
+
+        finally:
+            conn.close()
+            return merkinnat
+
+    # (kanta) ei ole tiedosto
     else:
-        print("kakkaa täh?")
-
-    conn.close()
-
-
-def test_connection():
-    try:
-        conn = sqlite3.connect(kanta)
-        if conn:
-            print("Yhteys toimii.")
-    except sqlite3.Error as e:
-        print("Ei toimi T_T", e)
-    finally:
         conn.close()
+        print("Tiedostoa ei löydy.")
